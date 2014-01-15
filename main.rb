86a358975fac515fe3c2a5ef8f9776684c087ac6
K Mak <karen.mak@stx.oxon.org>
2014-01-14 19:51:05 -0500

    added valid zip code, phone number, and non mit addresses

--- main.rb
+++ main.rb
@@ -4,29 +4,36 @@ require 'pry'
 def valid_ip_addresses(source_text)
   #Pattern\.Pattern\.Pattern\.Pattern
   valid_addresses = $source_text.scan(/(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/)
-  valid_addresses
 end
 
 def valid_mit_ip_addresses(source_text)
   valid_mit_addresses = $source_text.scan(/(?:(?:18?)\.)(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.)(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.)(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/)
-  valid_mit_addresses
 end
 
 def non_mit_ip_addresses(source_text)
-  valid_non_mit_addresses = $source_text.scan
+  valid_addresses = $source_text.scan(/(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/)
+  valid_mit_addresses = $source_text.scan(/(?:(?:18?)\.)(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.)(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.)(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/)
+
+  valid_non_mit_addresses = valid_addresses -valid_mit_addresses
+
+  valid_non_mit_addresses
+end
 
 def valid_phone_numbers(source_text)
   $source_text.scan(/[2-9]{1}\d{0,2}\-\d{0,3}\-?\d{4}/).uniq
 end
 
-def area_codes
+def area_codes(source_text)
+  valid_area_codes = $source_text.scan
 end
 
-def email_addresses
+def email_addresses(source_text)
+  valid_email_addresses = $source_text.scan
 end
 
-def zip_codes
+def zip_codes(source_text)
+  valid_zip_codes = $source_text.scan(/\b[0-9]{5}\b/)
 end
-
-def hex_colors
+def hex_colors(source_text)
+  valid_hex_colors = $source_text.scan
 end
