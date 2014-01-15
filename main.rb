5408d742cfddbb34be06eb50c1dcaf581a8d1785
K Mak <karen.mak@stx.oxon.org>
2014-01-14 20:16:34 -0500

    valid hex_colors

--- main.rb
+++ main.rb
@@ -24,16 +24,20 @@ def valid_phone_numbers(source_text)
 end
 
 def area_codes(source_text)
-  valid_area_codes = $source_text.scan
+  valid_area_codes = $source_text.scan(/[2-9][0-8][0-9]/)
 end
 
 def email_addresses(source_text)
-  valid_email_addresses = $source_text.scan
+  valid_email_addresses = $source_text.scan()
 end
 
 def zip_codes(source_text)
   valid_zip_codes = $source_text.scan(/\b[0-9]{5}\b/)
 end
 def hex_colors(source_text)
-  valid_hex_colors = $source_text.scan
+
+  #A-F, 0-9
+  valid_hex_colors = $source_text.scan(/(?<=#)[0-9a-f]{6}/)
 end
+
+
