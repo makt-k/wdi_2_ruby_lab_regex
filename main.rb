503a88981c9c48174bd5a936805a0839603c0a1a
K Mak <karen.mak@stx.oxon.org>
2014-01-14 19:28:29 -0500

    passed ip address and mit ip address methods

--- main.rb
+++ main.rb
@@ -1,26 +1,32 @@
 require_relative 'source'
 require 'pry'
 
-def valid_ip_addresses 
-end 
-
-def valid_mit_ip_addresses 
+def valid_ip_addresses(source_text)
+  #Pattern\.Pattern\.Pattern\.Pattern
+  valid_addresses = $source_text.scan(/(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/)
+  valid_addresses
 end
 
-def non_mit_ip_addresses 
-end 
-
-def valid_phone_numbers 
+def valid_mit_ip_addresses(source_text)
+  valid_mit_addresses = $source_text.scan(/(?:(?:18?)\.)(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.)(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.)(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/)
+  valid_mit_addresses
 end
 
-def area_codes 
+def non_mit_ip_addresses(source_text)
+  valid_non_mit_addresses = $source_text.scan
+
+def valid_phone_numbers(source_text)
+  $source_text.scan(/[2-9]{1}\d{0,2}\-\d{0,3}\-?\d{4}/).uniq
 end
 
-def email_addresses 
+def area_codes
 end
 
-def zip_codes 
+def email_addresses
 end
 
-def hex_colors 
+def zip_codes
+end
+
+def hex_colors
 end
